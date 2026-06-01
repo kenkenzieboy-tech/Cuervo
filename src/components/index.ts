539c9cd13d1c71f8394781cea600835ecef941b9
kenkenzieboy-tech <kenkenzieboy@gmail.com>
2026-06-01 07:56:32 +0700

    Add components index export

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Navbar } from './Navbar';
+export { Footer } from './Footer';
+export { ThemeToggle } from './ThemeToggle';
+export { WhatsAppWidget } from './WhatsAppWidget';
+export { HeroSection } from './HeroSection';
